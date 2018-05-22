dee45a423517b9b4081d76d60ebd82e99f4f6c1e
BorsicTeja <33715799+BorsicTeja@users.noreply.github.com>
2018-05-22 11:35:11 +0200

    Add files via upload
    Ta je sedaj malo popravljen.

---
+++ EkoloskaTrznica/src/main/java/si/feri/eko/EkoloskaTrznicaApplication.java
@@ -0,0 +1,12 @@
+package si.feri.eko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkoloskaTrznicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkoloskaTrznicaApplication.class, args);
+	}
+}
