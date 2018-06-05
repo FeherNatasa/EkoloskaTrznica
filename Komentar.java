d215e8724985969a0727b9908f04ee4cadbe5d16
KitekMaja <33865439+KitekMaja@users.noreply.github.com>
2018-06-05 12:15:53 +0200

    Add files via upload

---
+++ Komentar.java
@@ -0,0 +1,21 @@
+package si.feri.eko.baza;
+
+public class Komentar{
+	
+	private int id;
+	
+	private String vsebina;
+	
+	public Komentar(String vsebina)
+	{
+		this.vsebina = vsebina;
+	}
+	
+	public String getVsebina(){
+		return vsebina;
+	}
+	
+	public void setVsebina(String vsebina){
+		this.vsebina = vsebina;
+	}
+}
---
+++ sqlStavki.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS 'ekohiska'.'komentar'(
+	'idKomentar' INT NOT NULL AUTO_INCREMENT,
+	'vsebina' VARCHAR(200) NOT NULL,
+	PRIMARY KEY ('idKomentar'),
+	INDEX 'fk_komentar_kmetija_idx'('tk_idKmetija' ASC),
+	CONSTRAINT 'fk_komentar_kmetija'
+		FOREIGN KEY('tk_idKmetija')
+		REFERENCES 'ekohiska'.'kmetija'('idKmetija')
+		ON DELETE CASCADE
+		ON UPDATE NO ACTION,
+	INDEX 'fk_komentar_uporabnik_idx'('tk_idUporabnik' ASC)
+	CONSTRAINT 'fk_komentar_uporabnik'
+		FOREIGN KEY('tk_idUporabnik')
+		REFERENCES 'ekohiska'.'uporabnik'('idUporabnik')
+		ON DELETE CASCADE
+		ON UPDATE NO ACTION
+		)
+	ENGINE = InnoDB;
+)
