1953fa0f4c93a662ef6dd533515c95c1e55653ac
FeherNatasa <33715929+FeherNatasa@users.noreply.github.com>
2018-06-06 10:49:09 +0200

    Add files via upload

---
+++ EkoloskaTrznica/src/main/java/si/feri/eko/bazaRepositories/AbstractLobPreparedStatementCreator.java
@@ -0,0 +1,30 @@
+package si.feri.eko.bazaRepositories;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.lob.LobCreator;
+import org.springframework.jdbc.support.lob.LobHandler;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public abstract class AbstractLobPreparedStatementCreator implements PreparedStatementCreator {
+    private final LobHandler lobHandler;
+    private final String sql;
+    private final String keyColumn;
+
+    public AbstractLobPreparedStatementCreator(LobHandler lobHandler, String sql, String keyColumn) {
+        this.lobHandler = lobHandler;
+        this.sql = sql;
+        this.keyColumn = keyColumn;
+    }
+    public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+        PreparedStatement ps = con.prepareStatement(sql, new String[] { keyColumn });
+        LobCreator lobCreator = this.lobHandler.getLobCreator();
+        setValues(ps, lobCreator);
+        return ps;
+    }
+    protected abstract void setValues(PreparedStatement ps, LobCreator lobCreator) throws SQLException, DataAccessException;
+}
+
