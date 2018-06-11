1be33afeb50f66af18d15a872333493e13de225f
FeherNatasa <33715929+FeherNatasa@users.noreply.github.com>
2018-06-11 17:03:44 +0200

    Update SlikaDao.java

--- EkoloskaTrznica/src/main/java/si/feri/eko/bazaRepositories/SlikaDao.java
+++ EkoloskaTrznica/src/main/java/si/feri/eko/bazaRepositories/SlikaDao.java
@@ -91,4 +91,54 @@ public class SlikaDao {
        }
        return ret;
     }
+
+     public List<SlikaI> getSlikaIByFK(int id){
+        String sql = "SELECT * FROM slikai WHERE tk_idIzdelek = " + id;
+        List<SlikaI> ret = new ArrayList<SlikaI>();
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql);
+        for (Map<String, Object> row : rows){
+            byte[] blob = (byte[]) row.get("datoteka");
+            String retrieveBlobAsString = Base64.getEncoder().encodeToString(blob);
+
+            ret.add(new SlikaI(retrieveBlobAsString));
+        }
+        return ret;
+    }
+
+    public List<SlikaI> vrniSlikoIzdelka(){
+        String sql = "SELECT tk_idIzdelek, Datoteka from slikai where tk_idIzdelek in (select id_izdelek from izdelek) group by tk_idIzdelek;";
+        List<SlikaI> ret = new ArrayList<SlikaI>();
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql);
+        for (Map<String, Object> row : rows){
+            byte[]blob = (byte[]) row.get("datoteka");
+            String retrieveBlobAsString = Base64.getEncoder().encodeToString(blob);
+            int tk_idKmetija=(int)row.get("tk_idKmetija");
+
+            ret.add(new SlikaI(retrieveBlobAsString, tk_idKmetija));
+        }
+        return ret;
+    }
+
+    public void saveI(MultipartFile f, int tk_idIzdelek){
+        try{
+            LobHandler lobhandler = new DefaultLobHandler();
+            final File blobIn = convert(f);
+            final InputStream blobIs = new FileInputStream(blobIn);
+            jdbcTemplate.execute(
+                    "INSERT INTO slikai (Datoteka2, tk_idIzdelek) VALUES (?,?)",
+                    new AbstractLobCreatingPreparedStatementCallback(lobhandler) {
+                        protected void setValues(PreparedStatement ps, LobCreator lobCreator)
+                                throws SQLException {
+                            //ps.setLong(1,1L);
+                            lobCreator.setBlobAsBinaryStream(ps, 1, blobIs, (int) blobIn.length());
+                            ps.setString(2, ""+tk_idIzdelek);
+                        }
+                    }
+            );
+            blobIs.close();
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+    }
 }
